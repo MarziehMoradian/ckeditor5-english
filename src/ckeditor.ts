d48e78b9dc9e378d92f303d696fd08204db89fb6
co.moradian <marziehmoradiann@gmail.com>
2024-05-27 10:10:10 +0330

    add hashtag plugin

--- src/ckeditor.ts
+++ src/ckeditor.ts
@@ -3,110 +3,112 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
+import { ClassicEditor } from "@ckeditor/ckeditor5-editor-classic";
 
-import { Alignment } from '@ckeditor/ckeditor5-alignment';
-import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
-import { Autosave } from '@ckeditor/ckeditor5-autosave';
-import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles';
-import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
-import { CloudServices } from '@ckeditor/ckeditor5-cloud-services';
-import { CodeBlock } from '@ckeditor/ckeditor5-code-block';
-import type { EditorConfig } from '@ckeditor/ckeditor5-core';
-import { Essentials } from '@ckeditor/ckeditor5-essentials';
-import { FontColor, FontSize } from '@ckeditor/ckeditor5-font';
-import { Heading } from '@ckeditor/ckeditor5-heading';
-import { Highlight } from '@ckeditor/ckeditor5-highlight';
+import { Alignment } from "@ckeditor/ckeditor5-alignment";
+import { Autoformat } from "@ckeditor/ckeditor5-autoformat";
+import { Autosave } from "@ckeditor/ckeditor5-autosave";
+import { Bold, Italic } from "@ckeditor/ckeditor5-basic-styles";
+import { BlockQuote } from "@ckeditor/ckeditor5-block-quote";
+import { CloudServices } from "@ckeditor/ckeditor5-cloud-services";
+import { CodeBlock } from "@ckeditor/ckeditor5-code-block";
+import type { EditorConfig } from "@ckeditor/ckeditor5-core";
+import { Essentials } from "@ckeditor/ckeditor5-essentials";
+import { FontColor, FontSize } from "@ckeditor/ckeditor5-font";
+import { Heading } from "@ckeditor/ckeditor5-heading";
+import { Highlight } from "@ckeditor/ckeditor5-highlight";
 import {
-	Image,
-	ImageCaption,
-	ImageStyle,
-	ImageToolbar,
-	ImageUpload
-} from '@ckeditor/ckeditor5-image';
-import { Indent } from '@ckeditor/ckeditor5-indent';
-import { Link } from '@ckeditor/ckeditor5-link';
-import { List } from '@ckeditor/ckeditor5-list';
-import { MediaEmbed } from '@ckeditor/ckeditor5-media-embed';
-import { Mention } from '@ckeditor/ckeditor5-mention';
-import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
-import { PasteFromOffice } from '@ckeditor/ckeditor5-paste-from-office';
-import { TextTransformation } from '@ckeditor/ckeditor5-typing';
-import { Undo } from '@ckeditor/ckeditor5-undo';
-import { Base64UploadAdapter } from '@ckeditor/ckeditor5-upload';
+  Image,
+  ImageCaption,
+  ImageStyle,
+  ImageToolbar,
+  ImageUpload,
+} from "@ckeditor/ckeditor5-image";
+import { Indent } from "@ckeditor/ckeditor5-indent";
+import { Link } from "@ckeditor/ckeditor5-link";
+import { List } from "@ckeditor/ckeditor5-list";
+import { MediaEmbed } from "@ckeditor/ckeditor5-media-embed";
+import { Mention } from "@ckeditor/ckeditor5-mention";
+import { Paragraph } from "@ckeditor/ckeditor5-paragraph";
+import { PasteFromOffice } from "@ckeditor/ckeditor5-paste-from-office";
+import { TextTransformation } from "@ckeditor/ckeditor5-typing";
+import { Undo } from "@ckeditor/ckeditor5-undo";
+import { Base64UploadAdapter } from "@ckeditor/ckeditor5-upload";
+import HashtagPlugin from "./plugins/hashtag";
 
 // You can read more about extending the build with additional plugins in the "Installing plugins" guide.
 // See https://ckeditor.com/docs/ckeditor5/latest/installation/plugins/installing-plugins.html for details.
 
 class Editor extends ClassicEditor {
-	public static override builtinPlugins = [
-		Alignment,
-		Autoformat,
-		Autosave,
-		Base64UploadAdapter,
-		BlockQuote,
-		Bold,
-		CloudServices,
-		CodeBlock,
-		Essentials,
-		FontColor,
-		FontSize,
-		Heading,
-		Highlight,
-		Image,
-		ImageCaption,
-		ImageStyle,
-		ImageToolbar,
-		ImageUpload,
-		Indent,
-		Italic,
-		Link,
-		List,
-		MediaEmbed,
-		Mention,
-		Paragraph,
-		PasteFromOffice,
-		TextTransformation,
-		Undo
-	];
+  public static override builtinPlugins = [
+    Alignment,
+    Autoformat,
+    Autosave,
+    Base64UploadAdapter,
+    BlockQuote,
+    Bold,
+    CloudServices,
+    CodeBlock,
+    Essentials,
+    FontColor,
+    FontSize,
+    Heading,
+    Highlight,
+    Image,
+    ImageCaption,
+    ImageStyle,
+    ImageToolbar,
+    ImageUpload,
+    Indent,
+    Italic,
+    Link,
+    List,
+    MediaEmbed,
+    Mention,
+    Paragraph,
+    PasteFromOffice,
+    TextTransformation,
+    Undo,
+    HashtagPlugin,
+  ];
 
-	public static override defaultConfig: EditorConfig = {
-		toolbar: {
-			items: [
-				'heading',
-				'|',
-				'fontColor',
-				'fontSize',
-				'bold',
-				'italic',
-				'link',
-				'highlight',
-				'bulletedList',
-				'numberedList',
-				'|',
-				'outdent',
-				'indent',
-				'alignment',
-				'|',
-				'codeBlock',
-				'imageUpload',
-				'blockQuote',
-				'mediaEmbed',
-				'undo',
-				'redo'
-			]
-		},
-		language: 'en',
-		image: {
-			toolbar: [
-				'imageTextAlternative',
-				'toggleImageCaption',
-				'imageStyle:inline',
-				'imageStyle:block',
-				'imageStyle:side'
-			]
-		}
-	};
+  public static override defaultConfig: EditorConfig = {
+    toolbar: {
+      items: [
+        "heading",
+        "|",
+        "fontColor",
+        "fontSize",
+        "bold",
+        "italic",
+        "link",
+        "highlight",
+        "bulletedList",
+        "numberedList",
+        "|",
+        "outdent",
+        "indent",
+        "alignment",
+        "|",
+        "codeBlock",
+        "imageUpload",
+        "blockQuote",
+        "mediaEmbed",
+        "undo",
+        "redo",
+      ],
+    },
+    language: "fa",
+    image: {
+      toolbar: [
+        "imageTextAlternative",
+        "toggleImageCaption",
+        "imageStyle:inline",
+        "imageStyle:block",
+        "imageStyle:side",
+      ],
+    },
+  };
 }
 
 export default Editor;
---
+++ src/plugins/hashtag.ts
@@ -0,0 +1,68 @@
+// src/plugins/hashtag.js
+
+import Plugin from "@ckeditor/ckeditor5-core/src/";
+import TextWatcher from "@ckeditor/ckeditor5-typing/src/textwatcher";
+
+export default class HashtagPlugin extends Plugin {
+  static get pluginName() {
+    return "HashtagPlugin";
+  }
+
+  private getSuggestions: (text: string) => Promise<string[]>;
+
+  constructor(editor: any) {
+    super(editor);
+
+    // Get the getSuggestions function from the plugin options
+    this.getSuggestions = editor.config.get("hashtag.suggestions");
+  }
+
+  init() {
+    const editor = this.editor;
+    const viewDocument = editor.editing.view.document;
+    const model = editor.model;
+
+    const textWatcher = new TextWatcher(model, /#\w*$/);
+
+    textWatcher.on("matched", async (evt: any, data: any) => {
+      const matchedText = data.text;
+      const results = await this.getSuggestions(matchedText);
+      this.showSuggestions(results, data.range);
+    });
+
+    textWatcher.on("unmatched", () => {
+      this.hideSuggestions();
+    });
+
+    this._addSuggestionClickListener();
+  }
+
+  async getHashtagSuggestions(text: string) {
+    // Implementation to fetch hashtag suggestions from server
+    return [];
+  }
+
+  showSuggestions(suggestions: string[], range: Range) {
+    // Show suggestions logic
+  }
+
+  hideSuggestions() {
+    // Hide suggestions logic
+  }
+
+  _addSuggestionClickListener() {
+    // Add click listener logic
+  }
+
+  _insertHashtag(hashtag: string) {
+    const editor = this.editor;
+    const model = editor.model;
+
+    editor.model.change((writer) => {
+      const selection = model.document.selection;
+      const position = selection.getFirstPosition();
+
+      writer.insertText(hashtag, position);
+    });
+  }
+}
